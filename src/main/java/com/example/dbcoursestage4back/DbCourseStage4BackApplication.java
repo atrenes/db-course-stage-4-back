baa6e862a0aa64ae8c727147ba5cfcb4b9bcbdd8
atrenes <hanalainendm@gmail.com>
2023-01-11 15:50:57 +0300

    init spring app

---
+++ src/main/java/com/example/dbcoursestage4back/DbCourseStage4BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbcoursestage4back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbCourseStage4BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbCourseStage4BackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dbcoursestage4back/DbCourseStage4BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbcoursestage4back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbCourseStage4BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
